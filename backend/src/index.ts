4a9ce781f362061cd0388501916ab4e2a6ddba69
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-07-13 20:15:01 +0530

    Add membership purchase  route in the app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,16 @@
 import express from "express";
+import userAuthenticationRouter from "./routes/userRoutes/authenticationRoutes";
+import adminAuthenticationRouter from "./routes/adminRoutes/authenticationRoutes";
+import membershipPurchaseRouter from "./routes/userRoutes/membershipPurchaseRoutes";
+
 const app = express();
-import userAuthenticationRouter from "./routes/userRoutes/authenticationRoutes"
-import adminAuthenticationRouter from "./routes/adminRoutes/authenticationRoutes"
- 
+
 app.use(express.json());
 
-
 app.use("/users/authentication", userAuthenticationRouter);
-app.use("/admin/authentication",adminAuthenticationRouter);
+app.use("/admin/authentication", adminAuthenticationRouter);
+app.use("/users/membership/purchase", membershipPurchaseRouter);
 
-
-app.listen(3000, () => {
+app.listen(3050, () => {
   console.log("Listening on port 3000");
 });
